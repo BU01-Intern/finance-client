938767bedebfa753b67692dff3f52296a5b8ef30
minh do <minhquangdo201@gmail.com>
2023-08-14 16:46:14 +0700

    restore contractdetail.service.ts

--- src/app/service/contractdetail.service.ts
+++ src/app/service/contractdetail.service.ts
@@ -14,7 +14,7 @@ export class ContractdetailService {
       "start": "15/06/2023",
       "finish": "01/12/2023",
       // "money" : "100.000.000 VNĐ",
-      "status": 1,
+      "status": 0,
       "tech": [
         {
           "project": "VSS_FINANCE_A",
@@ -462,7 +462,7 @@ export class ContractdetailService {
     return []
   }
   getContractDetail(code: string) {
-    return this.contracts.find(contract => contract.code === code);
+    return this.contracts.find(contract => contract.code);
   }
   getCustomerById(code: string): Customer | any{
     const temp = this.contracts.find(contract => contract.code === code)
